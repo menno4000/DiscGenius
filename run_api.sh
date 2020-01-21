5fb9cbcdaa1716ae69c2795be370189864ace006
d064467 <oskar.sailer@sap.com>
2020-01-21 13:46:33 +0100

    additional setup

--- run_api.sh
+++ run_api.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+mkdir audio
+mkdir audio/songs
+mkdir audio/mixes
+
+export PYTHONPATH=${PYTHONPATH:-.}
+echo "PYTHONPATH set to ${PYTHONPATH}"
+
 pip install -r requirements.txt
 uvicorn discgenius.api:app --reload --port 9001
