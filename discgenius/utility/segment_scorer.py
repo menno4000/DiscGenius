1aea18f7e5dea1fbd7ca2e106f0a7041e1297168
Jeet <oskar.sailer@sap.com>
2020-03-28 11:35:01 +0100

    improve performance for segment scoring

--- discgenius/utility/segment_scorer.py
+++ discgenius/utility/segment_scorer.py
@@ -10,27 +10,26 @@ def calc_stft_averages(stft):
 
 #calculates the euclidean distance of two full signal stft ouputs
 def calc_euclidean_distance(stft1, stft2):
-    stft1_averages = calc_stft_averages(stft1)
-    stft2_averages = calc_stft_averages(stft2)
+
     euclidean_distances = []
-    for index in range(0, (len(stft1_averages)-1)):
-        euclidean_distances.append(distance.euclidean(stft1_averages[index], stft2_averages[index]))
+    for index in range(0, (len(stft1)-1)):
+        euclidean_distances.append(distance.euclidean(stft1[index], stft2[index]))
     return numpy.average(euclidean_distances)
 
 #calculates segment scores for a list of beat clips
 def score_segments(clips, segment_length, midpoint, bias_mode=False):
     scores = []
+
     #only compute scores for first or last third of song depending on bias mode
     if bias_mode:
+        clip_stfts = [calc_stft_averages(numpy.abs(librosa.stft(clips[i]))) for i in range(0, (int(len(clips)*0.33)+segment_length))]
         for si in range (0, (int(len(clips)*0.33))):
-            clip_stfts = [numpy.abs(librosa.stft(clips[i])) for i in range(si, (si+(segment_length-1)))]
-
             if midpoint > 2:
-                score_first = numpy.sum([calc_euclidean_distance(clip_stfts[0], clip_stfts[i]) for i in range(1, (midpoint-1))])
-                score_second = numpy.sum([calc_euclidean_distance(clip_stfts[0], clip_stfts[i]) for i in range(midpoint, (segment_length-1))])
+                score_first = numpy.sum([calc_euclidean_distance(clip_stfts[si], clip_stfts[si+i]) for i in range(1, (midpoint-1))])
+                score_second = numpy.sum([calc_euclidean_distance(clip_stfts[si], clip_stfts[si+i]) for i in range(midpoint, (segment_length-1))])
             else:
-                score_first = calc_euclidean_distance(clip_stfts[0], clip_stfts[1])
-                score_second = numpy.sum([calc_euclidean_distance(clip_stfts[0], clip_stfts[i]) for i in range(midpoint, (segment_length-1))])
+                score_first = calc_euclidean_distance(clip_stfts[si], clip_stfts[si+1])
+                score_second = numpy.sum([calc_euclidean_distance(clip_stfts[si], clip_stfts[si+i]) for i in range(midpoint, (segment_length-1))])
 
 
             score = score_first + score_second
@@ -39,17 +38,17 @@ def score_segments(clips, segment_length, midpoint, bias_mode=False):
         for si in range (int(len(clips)*0.33), (len(clips)-segment_length)):
             scores.append(100)
     else:
+        clip_stfts = [calc_stft_averages(numpy.abs(librosa.stft(clips[i]))) for i in range(0, len(clips))]
+
         for si in range (0, (int(len(clips)*0.66))):
             scores.append(100)
-        for si in range (int(len(clips)*0.66), (len(clips)-segment_length)):
-            clip_stfts = [numpy.abs(librosa.stft(clips[i])) for i in range(si, (si+(segment_length-1)))]
-
+        for si in range (int(len(clips)*0.66), (len(clips)-(segment_length))):
             if midpoint > 2:
-                score_first = numpy.sum([calc_euclidean_distance(clip_stfts[0], clip_stfts[i]) for i in range(1, (midpoint-1))])
-                score_second = numpy.sum([calc_euclidean_distance(clip_stfts[0], clip_stfts[i]) for i in range(midpoint, (segment_length-1))])
+                score_first = numpy.sum([calc_euclidean_distance(clip_stfts[si], clip_stfts[si+i]) for i in range(1, (midpoint-1))])
+                score_second = numpy.sum([calc_euclidean_distance(clip_stfts[si], clip_stfts[si+i]) for i in range(midpoint, (segment_length-1))])
             else:
-                score_first = calc_euclidean_distance(clip_stfts[0], clip_stfts[1])
-                score_second = numpy.sum([calc_euclidean_distance(clip_stfts[0], clip_stfts[i]) for i in range(midpoint, (segment_length-1))])
+                score_first = calc_euclidean_distance(clip_stfts[si], clip_stfts[si+1])
+                score_second = numpy.sum([calc_euclidean_distance(clip_stfts[si], clip_stfts[si+i]) for i in range(midpoint, (segment_length-1))])
 
 
             score = score_first + score_second
