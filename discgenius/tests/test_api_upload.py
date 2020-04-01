2f0a2f73cb29fc06ce5406766cdb7333ea305d80
Jeet <oskar.sailer@sap.com>
2020-04-01 16:42:12 +0200

    fix upload tests

--- discgenius/tests/test_api_upload.py
+++ discgenius/tests/test_api_upload.py
@@ -8,8 +8,10 @@ from starlette.testclient import TestClient
 from ..api import app
 from ..utility import common
 
-EXAMPLE_AUDIO = "mini_audio.wav"
-EXAMPLE_AUDIO_PATH = f"discgenius/tests/{EXAMPLE_AUDIO}"
+EXAMPLE_AUDIO = "mini-audio"
+WAV_EXTENSION = 'wav'
+WAV_AUDIO_PATH = f"discgenius/tests/{EXAMPLE_AUDIO}.{WAV_EXTENSION}"
+BPM = 124.36
 
 
 class TestAPIUpload(unittest.TestCase):
@@ -17,7 +19,7 @@ class TestAPIUpload(unittest.TestCase):
     def setUp(self):
         self.config = common.get_config("./content.ini")
         self.client = TestClient(app)
-        file = wave.open(EXAMPLE_AUDIO_PATH, 'rb')
+        file = wave.open(WAV_AUDIO_PATH, 'rb')
         self.example_audio_wav = file.readframes(file.getnframes())
 
     def tearDown(self):
@@ -29,21 +31,21 @@ class TestAPIUpload(unittest.TestCase):
         assert 400 == response.status_code
 
     def test_upload_no_filename(self):
-        url_params = "extension=wav"
+        url_params = f"extension={WAV_EXTENSION}"
         response = self.client.post(f"/upload?{url_params}", self.example_audio_wav)
         assert 400 == response.status_code
         assert "provide" in response.json()['detail']
         assert "filename" in response.json()['detail']
 
     def test_upload_no_extension(self):
-        url_params = "filename=mini_audio.wav"
+        url_params = f"filename={EXAMPLE_AUDIO}"
         response = self.client.post(f"/upload?{url_params}", self.example_audio_wav)
         assert 400 == response.status_code
         assert "provide" in response.json()['detail']
         assert "extension" in response.json()['detail']
 
     def test_upload_no_audio_file(self):
-        url_params = "filename=mini_audio.wav&extension=wav"
+        url_params = f"filename={EXAMPLE_AUDIO}&extension={WAV_EXTENSION}&bpm={BPM}"
         response = self.client.post(f"/upload?{url_params}")
         assert 400 == response.status_code
         assert "provide" in response.json()['detail']
@@ -51,42 +53,36 @@ class TestAPIUpload(unittest.TestCase):
         assert "file" in response.json()['detail']
 
     def test_filename_has_no_format(self):
-        filename = "mini_audio"
-        extension = "wav"
-        url_params = f"filename={filename}&extension={extension}"
+        url_params = f"filename={EXAMPLE_AUDIO}&extension={WAV_EXTENSION}&bpm={BPM}"
         response = self.client.post(f"/upload?{url_params}", self.example_audio_wav)
         assert 200 == response.status_code
         print(response.json())
-        assert f"{filename}.{extension}" == response.json()['filename']
+        assert f"{EXAMPLE_AUDIO}_{BPM}.{WAV_EXTENSION}" == response.json()['filename']
 
     def test_filename_shorter_4(self):
         filename = "min"
-        extension = "wav"
-        url_params = f"filename={filename}&extension={extension}"
+        url_params = f"filename={filename}&extension={WAV_EXTENSION}&bpm={BPM}"
         response = self.client.post(f"/upload?{url_params}", self.example_audio_wav)
         assert 200 == response.status_code
-        assert f"{filename}.{extension}" == response.json()['filename']
+        assert f"{filename}_{BPM}.{WAV_EXTENSION}" == response.json()['filename']
 
     def test_same_filename(self):
-        filename = "mini"
-        extension = "wav"
-        url_params = f"filename={filename}&extension={extension}"
+        url_params = f"filename={EXAMPLE_AUDIO}&extension={WAV_EXTENSION}&bpm={BPM}"
         response = self.client.post(f"/upload?{url_params}", self.example_audio_wav)
         assert 200 == response.status_code
-        assert f"{filename}.{extension}" == response.json()['filename']
+        assert f"{EXAMPLE_AUDIO}_{BPM}.{WAV_EXTENSION}" == response.json()['filename']
 
         response = self.client.post(f"/upload?{url_params}", self.example_audio_wav)
         assert 200 == response.status_code
-        assert not f"{filename}.{extension}" == response.json()['filename']
-        assert f"{filename}-1.{extension}" == response.json()['filename']
+        assert not f"{EXAMPLE_AUDIO}_{BPM}.{WAV_EXTENSION}" == response.json()['filename']
+        assert f"{EXAMPLE_AUDIO}-1_{BPM}.{WAV_EXTENSION}" == response.json()['filename']
 
     def test_upload_mp3_creates_wav(self):
-        filename = "mini_audio"
         extension = "mp3"
-        url_params = f"filename={filename}&extension={extension}"
-        example_audio_mp3 = open(f"discgenius/tests/{filename}.{extension}", 'rb').read()
+        url_params = f"filename={EXAMPLE_AUDIO}&extension={extension}&bpm={BPM}"
+        example_audio_mp3 = open(f"discgenius/tests/{EXAMPLE_AUDIO}.{extension}", 'rb').read()
         response = self.client.post(f"/upload?{url_params}", example_audio_mp3)
         assert 200 == response.status_code
-        assert f"{filename}.wav" == response.json()['filename']
-        assert os.path.isfile(f"{self.config['song_path']}/{filename}.mp3")
-        assert os.path.isfile(f"{self.config['song_path']}/{filename}.wav")
+        assert f"{EXAMPLE_AUDIO}_{BPM}.{WAV_EXTENSION}" == response.json()['filename']
+        assert os.path.isfile(f"{self.config['mp3_storage']}/{EXAMPLE_AUDIO}_{BPM}.mp3")
+        assert os.path.isfile(f"{self.config['song_path']}/{EXAMPLE_AUDIO}_{BPM}.wav")
