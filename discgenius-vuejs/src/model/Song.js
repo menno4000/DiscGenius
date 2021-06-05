8ac9012bc877fa9bc1602935420de596711b7b5b
menno4000 <maxluedemann@t-online.de>
2021-06-05 17:50:56 +0200

    migrate frontend repo

--- discgenius-vuejs/src/model/Song.js
+++
@@ -1,8 +0,0 @@
-export default class Song {
-    constructor(title, length, tempo, id) {
-        this.title = title;
-        this.length = length;
-        this.tempo = tempo;
-        this.id = id;
-    }
-}
